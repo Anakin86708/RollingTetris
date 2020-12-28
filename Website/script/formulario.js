664ebc12e656ddcafa250cad1c024dd0f0d73f9f
Leonardo Teixeira <leeo_skw@hotmail.com>
2020-12-28 17:52:36 -0300

    Validação via cliente.

--- Website/script/formulario.js
+++ Website/script/formulario.js
@@ -15,3 +15,28 @@ function playerCadastro(){
     }
     mydatabase.push(basedados);
 }
+
+function validaFormulario()
+{
+    let nome = document.forms["form"]["nome"].value;
+    let aniversario = document.forms["form"]["aniversario"].value;
+    let cpf = document.forms["form"]["cpf"].value;
+    let telefone = document.forms["form"]["phone"].value;
+    let email = document.forms["form"]["mail"].value;
+    let usuario = document.forms["form"]["user"].value;
+    let senha = document.forms["form"]["senha"].value;
+
+    let validaCPF = "\d{11}";
+
+
+
+    if (nome == null || nome == "")
+    {
+        alert("Nome errado!");
+        return false;
+    }else if(aniversario == null)
+    {
+        alert("Data de nascimento deve ser válida");
+        return false;
+    }
+}
